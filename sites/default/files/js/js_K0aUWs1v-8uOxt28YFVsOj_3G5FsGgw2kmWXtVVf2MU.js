eed9c0bcc6eae9dd697623602459e58e0358ecf3
Diego Pinto <diego.pinto@bkjdigital.com>
2025-11-07 16:22:19 -0600

    for agencys new page

---
+++ sites/default/files/js/js_K0aUWs1v-8uOxt28YFVsOj_3G5FsGgw2kmWXtVVf2MU.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="refresh" content="0;url='/sites/default/files/js/js_bNtwSM06bdOZd_nxz0-L2hw-UZGQqIRAjwwAk6Wc-QA.js?scope=footer&amp;delta=0&amp;language=en&amp;theme=brightplum&amp;include=eJxLKspMzygpyCnN1U_PyU9KzNEtLqnMycxLBwCDaAoW'" />
+
+        <title>Redirecting to /sites/default/files/js/js_bNtwSM06bdOZd_nxz0-L2hw-UZGQqIRAjwwAk6Wc-QA.js?scope=footer&amp;delta=0&amp;language=en&amp;theme=brightplum&amp;include=eJxLKspMzygpyCnN1U_PyU9KzNEtLqnMycxLBwCDaAoW</title>
+    </head>
+    <body>
+        Redirecting to <a href="/sites/default/files/js/js_bNtwSM06bdOZd_nxz0-L2hw-UZGQqIRAjwwAk6Wc-QA.js?scope=footer&amp;delta=0&amp;language=en&amp;theme=brightplum&amp;include=eJxLKspMzygpyCnN1U_PyU9KzNEtLqnMycxLBwCDaAoW">/sites/default/files/js/js_bNtwSM06bdOZd_nxz0-L2hw-UZGQqIRAjwwAk6Wc-QA.js?scope=footer&amp;delta=0&amp;language=en&amp;theme=brightplum&amp;include=eJxLKspMzygpyCnN1U_PyU9KzNEtLqnMycxLBwCDaAoW</a>.
+    </body>
+</html>
